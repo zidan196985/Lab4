1396381e764ccee93e8bff9e78b2b473eac0ada5
z.baxtiyorov <z.baxtiyorov@newuu.uz>
2024-09-18 12:32:29 +0500

    git init

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+//PProblem 1
+//    int a;
+//    cin>>a;
+//    if (a>=0)
+//        cout<<"Positive!";
+//        else cout<<"Negative";
+//
+//
+//Problem 2
+
+//    int a;
+//    cin>>a;
+//    if (a>=0) {
+//        if (a % 2 == 0) {
+//            cout << "Positive and Even!";
+//        } else { cout << "Positive and Odd!"; }
+//    }
+//    else {
+//        if (a % 2 == 0) {
+//            cout << "Negative And Even"; }
+//        else { cout << "Negative And Odd";}
+//
+//    }
+//
+//Problem 3
+//
+//float a, b, c;
+//cout<<"Enter three Numbers:";
+//
+//cin>>a>>b>>c;
+//
+//if ((a>b)&&(a>c))
+//    cout<<"a= largest";
+//    else if ((b>a)&&(b>c))
+//        cout<<"b = largest";
+//    else
+//        cout<<"c = largest";
+// Problem 4
+
+//
+//int a, b; cout<<"Enter two numbers: "; cin>>a>>b;
+//
+//if (a%b==0) {
+//    cout<<"A is divisible by B";
+//}
+//else { cout<<"A is not divisible by b"; }
+
+//Problem 5
+
+//int a;
+//cout<<"Enter teh YEAR!!!";
+//cin>>a;
+//if (a%4==0) {
+//    if ((a % 100 == 0) && (a % 400 != 0)) {
+//
+//        cout<<"Not a leap year :(";}
+//    else {cout<<"Leap YEAR!!";}
+//}
+//else {cout<<"Not a leap year :(";}
+//Problem 6
+//
+//float a, b, c, d, x1, x2; cout<<"Enter numbres in the order of a, b, c: "; cin>>a>>b>>c;
+//d=sqrt(b*b-4*a*c);
+//
+//    x1=(-1*b-d)/(2*a);
+//    x2=(-1*b+d)/(2*a);
+//
+//if (d>0)
+//    cout<<"first x= "<<x1<<"\nsecond = "<<x2;
+//
+//else if (d==0)
+//        cout<<"x ="<<x1;
+//else
+//        cout<<"doesnt exist";
+// Problem 7
+//
+//float a, b, c; cin>>a>>b>>c;
+//if (a+b+c==180) cout<<"YES";
+//else cout<<"NO!";
+//Problem 8
+
+//char a='A'; cin>>a;
+//int b=a;
+//
+//if ((b>=65)&&(b<=90)) {
+//    cout<<"UpperCASE!";
+//}
+//else if ((b>=97)&&(b<=122)) {
+//    cout<<"LowerCase!";
+//}
+//else cout<<"Not Alphabet";
+//Problem 9
+
+
+
+
+return 0;
+}
